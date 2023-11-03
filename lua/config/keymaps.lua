d334eaa93334b318299b6999fd71a57d0cae533e
ICBench-chengrang <chengrang@icbench.com>
2023-11-03 18:06:59 +0800

    change keymap and options

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+keymap.set({'i', 'n'}， '<C-o>', '', {silent = true, remap = false})
+keymap.set("i", "<C-o>", "<Esc>")
+keymap.set("n", "<C-o><C-o>", ":w<CR>", {silent = true, remap = false})
