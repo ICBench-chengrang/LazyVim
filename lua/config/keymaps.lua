331056564f37222903b4a7308b9184fd2a0e435b
ICBench-chengrang <chengrang@icbench.com>
2024-04-15 18:25:14 +0800

    init keymap and option, add colorscheme

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+keymap.set("i", "<C-l>", "<Right>")
+
+-- keymap.set({ "i", "v" }, "<C-k>", "<esc>l", { silent = true, remap = false })
+keymap.set("n", "<C-k><C-k>", "<cmd>w<cr>", { silent = true, remap = false })
