5d5361e3c6e946966c1a94034ada5dcfce8c589f
ICBench-chengrang <chengrang@icbench.com>
2024-04-19 11:54:37 +0800

    add lsp.lua

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,15 @@
+return {
+  -- LSP keymaps
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- disable a keymap
+      keys[#keys + 1] = { "<C-k>", mode = "i", false }
+      -- change a keymap
+      keys[#keys + 1] = { "<C-k>", mode = "i", "<esc>l" }
+      -- add a keymap
+      keys[#keys + 1] = { "<C-j>", mode = { "i", "n" }, "<cmd>lua vim.lsp.buf.signature_help()<cr>" }
+    end,
+  },
+}
